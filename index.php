6195a719ca44a6e4b26b15b9d795b6e9288a7862
queratg <gwendal.querat@gmail.com>
2018-04-14 18:54:50 +0100

    layout, images and informations added

---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Gwendal</title>
+    <meta charset="UTF-8"/>
+    <meta name=viewport content="width=device-width, initial-scale=1">
+    <meta name="mobile-web-app-capable" content="yes">
+    <link rel="stylesheet" href="style/semantic.min.css">
+    <link rel="stylesheet" href="style/cv.css">
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/semantic.min.js"></script>
+</head>
+<body>
+
+<div class="ui fixed inverted menu" style="background-color: #00cab7">
+    <div class="ui container">
+        <a href="#" class="header item">
+            <!--<img class="logo" src="res/images/logo.png"/>-->
+            Gwendal QUERAT
+        </a>
+    </div>
+</div>
+
+<header class="ui container color2">
+    <div class="ui item">
+        <div class="ui grid middle aligned color2 white-text">
+            <div class="column eight wide center aligned">
+                <h1 class="title">Gwendal QUERAT</h1>
+                <h3 class="title">Software Developer</h3>
+                <!--<div class="ui container left aligned">-->
+                <!--</div>-->
+            </div>
+            <div class="column eight wide>">
+                <img class="ui left aligned image circular" src="res/gwen.png"/>
+            </div>
+        </div>
+    </div>
+</header>
+
+<div class="ui container">
+    <h1 class="header">About</h1>
+    <p>My favorite town is Jerusalem because Sunseille told me so</p>
+
+    <hr/>
+</div>
+
+<div class="ui container">
+
+    <div class="ui two column grid text">
+        <div class="column">
+            <h1 class="title">Formation</h1>
+            <div class="ui container">
+                <div class="ui grid">
+                    <div class="row">
+                        <div class="image three wide column">
+                            <img src="res/epitech.png" class="image" height="64"/>
+                        </div>
+                        <div class="twelve wide column">
+                            <span class="cv-title">Epitech</span>
+                            <span><i>&nbsp-&nbspParis Graduate School of Digital Innovation</i></span>
+                            <br/>
+                            <i>Paris, France</i>
+                            <br/>
+                            2014-2019
+                            <br/>
+                            Graduate Diploma in Information Technology
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="image three wide column middle aligned">
+                            <img src="res/ste-therese.jpg" class="image" height="48"/>
+                        </div>
+                        <div class="twelve wide column">
+                            <span class="title cv-title">Sainte Thérèse High School</span>
+                            <span><i>&nbsp-&nbsp Catering school</i></span>
+                            <br/>
+                            <span><i>La Guerche De Bretagne, France</i></span>
+                            <br/>
+                            2006-2010
+                            <br/>
+                            <span>Mention Complémentaire Cuisinier en Desserts de Restaurant</span>
+                            <br>
+                            <span>BEP & BAC Pro - Hotels and Catering Professions</span>
+                            <br>
+                            <span>Complementary Mention Restaurant Desserts Cook</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <h1 class="header">Skills</h1>
+            <p>We spend so much of our life looking - but never seeing. This is gonna be a happy little seascape. You
+                can do
+                anything your heart can imagine. If you've been in Alaska less than a year you're a Cheechako.</p>
+            <p>So often we avoid running water, and running water is a lot of fun. I really believe that if you practice
+                enough you could paint the 'Mona Lisa' with a two-inch brush. How do you make a round circle with a
+                square
+                knife? That's your challenge for the day. The light is your friend. Preserve it. This is the way you
+                take
+                out your flustrations. This is a happy place, little squirrels live here and play.</p>
+        </div>
+    </div>
+
+    <hr/>
+
+    <div class="ui container">
+        <h1>Professional Experience</h1>
+
+    </div>
+</div>
+</body>
+</html>
