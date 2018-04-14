e634e26505623bfb0bacb691b0baa4db30c2464a
queratg <gwendal.querat@gmail.com>
2018-04-14 19:36:03 +0100

    more content

--- index.php
+++ index.php
@@ -79,36 +79,109 @@
                             <br/>
                             2006-2010
                             <br/>
-                            <span>Mention Complémentaire Cuisinier en Desserts de Restaurant</span>
-                            <br>
                             <span>BEP & BAC Pro - Hotels and Catering Professions</span>
                             <br>
                             <span>Complementary Mention Restaurant Desserts Cook</span>
+                            <br>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="column">
-            <h1 class="header">Skills</h1>
-            <p>We spend so much of our life looking - but never seeing. This is gonna be a happy little seascape. You
-                can do
-                anything your heart can imagine. If you've been in Alaska less than a year you're a Cheechako.</p>
-            <p>So often we avoid running water, and running water is a lot of fun. I really believe that if you practice
-                enough you could paint the 'Mona Lisa' with a two-inch brush. How do you make a round circle with a
-                square
-                knife? That's your challenge for the day. The light is your friend. Preserve it. This is the way you
-                take
-                out your flustrations. This is a happy place, little squirrels live here and play.</p>
-        </div>
+
+
     </div>
 
     <hr/>
 
     <div class="ui container">
+        <h1 class="header">Skills</h1>
+        <p>We spend so much of our life looking - but never seeing. This is gonna be a happy little seascape. You
+            can do
+            anything your heart can imagine. If you've been in Alaska less than a year you're a Cheechako.</p>
+        <p>So often we avoid running water, and running water is a lot of fun. I really believe that if you practice
+            enough you could paint the 'Mona Lisa' with a two-inch brush. How do you make a round circle with a
+            square
+            knife? That's your challenge for the day. The light is your friend. Preserve it. This is the way you
+            take
+            out your flustrations. This is a happy place, little squirrels live here and play.</p>
+    </div>
+
+    <hr>
+
+    <div class="ui container">
         <h1>Professional Experience</h1>
+        <div class="ui grid">
+            <div class="row">
+                <div class="column two wide">
+                    <img class="image" src="res/ca.png" alt="ContentArmor" height="64px"/>
+                </div>
+                <div class="column fourteen wide">
+                    <div class="cv-title">ContentArmor</div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="ui container">
+        <hr>
+        <h1 class="title">Personal Projects</h1>
+        <p>
+            When you do it your way you can go anywhere you choose. Just take out whatever you don't want. It'll change
+            your entire perspective. It's almost like something out of a fairytale book. We have no limits to our world.
+            We're only limited by our imagination. This is your creation - and it's just as unique and special as you
+            are.
+        </p>
+        <p>
+            Let's have a happy little tree in here. That is when you can experience true joy, when you have no fear.
+            Absolutely no pressure. You are just a whisper floating across a mountain. It's cold, but it's
+            beautiful.
+        </p>
+
+        <p>
+            We'll take a little bit of Van Dyke Brown. Just pretend you are a whisper floating across a mountain. It's a
+            good way of getting rid of all your anxieties and hostilities. Fluff it up a little and hypnotize it. There
+            we go. Let that brush dance around there and play.
+        </p>
+    </div>
+
+    <div class="ui container">
+        <hr>
+        <h1 class="title">Hobbies</h1>
+        <p>
+            You can do anything your heart can imagine. You can create anything that makes you happy. We have a fantastic
+            little sky!
+        </p>
+
+        <p>
+            You want your tree to have some character. Make it special. Water's like me. It's laaazy ... Boy, it always
+            looks for the easiest way to do things Trees grow in all kinds of ways. They're not all perfectly straight.
+            Not every limb is perfect.
+        </p>
+
+        <p>
+            Take your time. Speed will come later. Be careful. You can always add more - but you can't take it away.
+            There is immense joy in just watching - watching all the little creatures in nature. As trees get older they
+            lose their chlorophyll.
+        </p>
+
+        <p>
+            It looks so good, I might as well not stop. Nothing's gonna make your husband or wife madder than coming home
+            and having a snow-covered dinner. Just use the old one inch brush. Here we're limited by the time we
+            have.
+        </p>
+
+        <p>
+            I want everbody to be happy. That's what it's all about. Isn't that fantastic that you can create an almighty
+            tree that fast? That's the way I look when I get home late; black and blue.
+        </p>
 
     </div>
+
+    <div class="ui container">
+        <hr>
+    </div>
+
 </div>
 </body>
 </html>
